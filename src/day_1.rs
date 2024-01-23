4bef1fad4f3b834c24a699bb659bdaf62d4768c8
FruitPunchSamurai <spatel752@gatech.edu>
2024-01-22 23:19:57 -0500

    Finish CLI arg logic and add boiler plate for day 1

---
+++ src/day_1.rs
@@ -0,0 +1,4 @@
+pub fn solve(input: &str) {
+    println!("Solving day 1 problem...");
+    println!("{}", input);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
+use std::fs;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long, value_delimiter = ',', num_args = 1..)]
-    days: Vec<i32>
+    #[arg(short = 'd', long, value_delimiter = ',', num_args = 1.., help = "Takes in a comma seperated list of integers representing the days to test")]
+    days: Vec<i32>,
 }
 
 
-
 fn main() {
-    let args =  Args::parse();
-    println!("Args is {:#?}", args);
+    let args = Args::parse();
+
+    let days;
+
+    if !args.days.is_empty() {
+        days = args.days;
+    } else {
+        days = (1..=25).collect();
+    }
+
+    for day in &days {
+        println!("Day {}", day);
+        let input_file_path = format!("./data/day_{}.txt", day);
+        let data = fs::read_to_string(&input_file_path);
+        if let Ok(data) = data {
+            let data = data.trim();
+            let func_to_run = match day {
+                1 => advent_of_code_2023::day_1::solve,
+                _ => unreachable!(),
+            };
+            func_to_run(data);
+            println!();
+        }
+    }
 }
